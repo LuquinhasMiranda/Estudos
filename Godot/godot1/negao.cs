89c8587c6343678ed3ebdbeba3cad78857967166
LuquinhasMiranda <LucasMirandaGiannella02@gmail.com>
2021-08-30 17:59:23 -0300

    Updates no proj do godot

--- Godot/godot1/negao.cs
+++ Godot/godot1/negao.cs
@@ -7,12 +7,13 @@ public class negao : KinematicBody
     const int KEY_S = 83;
     const int KEY_D = 68;
     const int KEY_A = 65;
+    const int KEY_SHIFT = 16777237;
 
     int MOVE_X = 0;
     int MOVE_Z = 0;
 
-    const int HORIZONTAL_SPEED = 20;
-    const int HORIZONTAL_SPEED_MOD = 20;
+    const float HORIZONTAL_SPEED = 20;
+    const float HORIZONTAL_SPEED_MOD = 1.8f;
 
     Vector3 vel = new Vector3();
 
@@ -23,16 +24,22 @@ public class negao : KinematicBody
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta) {
-        if (Input.IsKeyPressed(KEY_W)) MOVE_Z++;
-        if (Input.IsKeyPressed(KEY_S)) MOVE_Z--;
+        if (Input.IsKeyPressed(KEY_W)) MOVE_Z--;
+        if (Input.IsKeyPressed(KEY_S)) MOVE_Z++;
         if (Input.IsKeyPressed(KEY_D)) MOVE_X++;
         if (Input.IsKeyPressed(KEY_A)) MOVE_X--;
-        GD.Print(MOVE_Z + "" + MOVE_X);
 
         vel.x = MOVE_X * delta * HORIZONTAL_SPEED;
         vel.z = MOVE_Z * delta * HORIZONTAL_SPEED;
 
         vel = vel.Normalized();
+
+        if (Input.IsKeyPressed(KEY_SHIFT)){
+            vel.x = vel.x * HORIZONTAL_SPEED_MOD;
+            vel.y = vel.y * HORIZONTAL_SPEED_MOD;
+            GD.Print("shift");
+        }
+
 
         vel = MoveAndSlide(vel, Vector3.Up);
 
